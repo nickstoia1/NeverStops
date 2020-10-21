381e29c65ef049d4a1d63074c00dc91d30b6b11b
{name} <{nickstoia1@ou.edu}>
2020-10-21 16:32:50 -0500

    Make a stack

---
+++ src/SuperStack.java
@@ -0,0 +1,35 @@
+
+public class SuperStack<T>{
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T>(int size){
+		items = (T[]) new Object[size];
+	}
+	
+	public boolean isEmpty() {
+		//TODO
+		return true;
+	}
+	
+	public boolean isFull() {
+		//TODO
+		return true;
+	}
+	
+	public boolean push(T item) {
+		//TODO
+		return true;
+	}
+	
+	public T pop() {
+		// TODO
+		return null;
+	}
+	
+	public void displayElements() {
+		// TODO
+	}
+	
+
+}
